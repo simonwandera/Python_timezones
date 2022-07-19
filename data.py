09838bb8fa6f656bbc025e78527bcf6bff746fd1
simonwandera <simonwandera12@gmail.com>
2022-07-19 17:39:26 +0300

    Final commit

--- data.py
+++ data.py
@@ -1,13 +1,10 @@
 from datetime import datetime
 from pytz import timezone, all_timezones
 
-timeformat = '%d-%m-%Y, %H:%M:%S'
+def currentTime():
+    timeformat = '%d-%m-%Y, %H:%M:%S'
 
-now = datetime.now().strftime(timeformat)
+    now = datetime.now().strftime(timeformat)
 
-timeInArabia = datetime.now(timezone(zone='Asia/Riyadh')).strftime(timeformat)
-
-print(timeInArabia)
-
-
-print(now)
+    current_time = datetime.now(timezone(zone='Asia/Riyadh')).strftime(timeformat)
+    return
